70e4fbd2d9bffd6b60dc29453a6f55516f1af671
Gary Dix <intelproof@gmail.com>
2025-03-19 11:36:54 +1300

    Add project files.

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HelloWorld_ASP_Razor_2.Pages
+{
+
+    [BindProperties]
+    public class IndexModel : PageModel
+    {
+        //  public string Name;  //fields
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public string Result { get; set; }
+
+
+
+        public void OnGet()
+        {
+
+        }
+
+
+        public void OnPost()
+        {
+
+            Result = $"Hello {Name}, you are {Age} years old";
+
+        }
+
+
+    }
+}
